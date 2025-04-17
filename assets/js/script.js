dbd530e524c30512757c15ac632eb34a4ff33370
Megan Ellman <meganlellman@gmail.com>
2025-04-17 15:32:26 -0700

    save button saves to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,7 +27,7 @@ async function getConversion(val_1, currency_2El, currency_1) {
         console.log(json);
 
         // take the currency_2 and find the matching key and it's corresponding value in "conversion_rates", let conversionRate = this value
-        let currency_2 = document.querySelector('#currency_2').value
+        let currency_2 = document.querySelector('#currency_2').value;
         let conversionRate = json.conversion_rates[currency_2];
 
         // take currency-val_1 and multiply it by conversionRate--- let this = currency_2El
@@ -41,36 +41,58 @@ async function getConversion(val_1, currency_2El, currency_1) {
 
         // saveBtn display toggle ON
         saveBtn.setAttribute('style', 'display:inline-block');
-        saveBtn.addEventListener('click', function(){
-            console.log('here')
-            let tableRow = document.createElement('tr');
-            
-        
-            let currency_1_dashboard = document.createElement('td');
-            let currency_2_dashboard = document.createElement('td');
-            let deleteBtn = document.createElement('button');
-        
-            currency_1_dashboard.textContent = currency_1;
-            currency_2_dashboard.textContent = currency_2;
-            deleteBtn.textContent = 'Delete';
-            deleteBtn.setAttribute('onclick', ' return this.parentNode.remove()');
-        
-            tableRow.append(currency_1_dashboard);
-            tableRow.append(currency_2_dashboard);
-            tableRow.append(deleteBtn);
-            dashboardBody.append(tableRow);
-        })
     } catch (error) {
         console.error(error.message);
     }
 }
+saveBtn.addEventListener('click', () => saveData())
 
-dashboardBody.addEventListener('click', function(e){
+dashboardBody.addEventListener('click', function (e) {
     e.stopPropagation();
-    let currency_1 =  e.target.parentNode.children[0].textContent;
+    let currency_1 = e.target.parentNode.children[0].textContent;
     let currency_2 = e.target.parentNode.children[1].textContent;
-    
+
 
     document.getElementById('currency_1').value = currency_1;
     document.getElementById('currency_2').value = currency_2;
 })
+
+function saveData() {
+    console.log("saveData was triggered")
+    let currency_1 = document.getElementById('currency_1').value;
+    let currency_2 = document.querySelector('#currency_2').value;
+
+    let currencyData = {
+        currency1: currency_1,
+        currency2: currency_2
+    }
+
+    if (!(localStorage.getItem('dashboardData'))) {
+        console.log('no localStorage yet')
+        let dashboardData = [];
+        dashboardData.push(currencyData);
+        localStorage.setItem('dashboardData', JSON.stringify(dashboardData));
+    } else {
+        dashboardData = JSON.parse(localStorage.getItem('dashboardData'));
+        dashboardData.push(currencyData);
+        localStorage.setItem('dashboardData', JSON.stringify(dashboardData));
+    }
+
+    let tableRow = document.createElement('tr');
+
+
+    let currency_1_dashboard = document.createElement('td');
+    let currency_2_dashboard = document.createElement('td');
+    let deleteBtn = document.createElement('button');
+
+    currency_1_dashboard.textContent = currency_1;
+    currency_2_dashboard.textContent = currency_2;
+    deleteBtn.textContent = 'Delete';
+    deleteBtn.setAttribute('onclick', ' return this.parentNode.remove()');
+
+    tableRow.append(currency_1_dashboard);
+    tableRow.append(currency_2_dashboard);
+    tableRow.append(deleteBtn);
+    dashboardBody.append(tableRow);
+}
+
